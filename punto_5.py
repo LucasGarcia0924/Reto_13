e5b71705da654f8939019eeba9dd9904e04447a1
LucasGarcia0924 <159032760+LucasGarcia0924@users.noreply.github.com>
2024-09-21 16:12:05 -0500

    Update punto_5.py

--- punto_5.py
+++ punto_5.py
@@ -3,92 +3,52 @@ import json
 import requests
 from datetime import datetime
 
-# Se hace la petición a la API de bromas y se entrega el contenido
+# Se hace la petición a la API de bromas, se imprime el JSON y se entrega el contenido
 def broma(urls: list):
-    print(urls[0])
+    print(f"\n{urls[0]}\n")
     peticion = requests.get(urls[0])
-    print(peticion)
+    print(f"{peticion}\n")
     return json.loads(peticion.content)
 
-# Se hace la petición a la API de bitcoin y se entrega el contenido
-def bitcoinData(urls: list):
-    print(urls[1])
+# Se hace la petición a la API de perros, se imprime el JSON y se entrega el contenido
+def perros(urls: list):
+    print(f"\n{urls[1]}\n")
     peticion = requests.get(urls[1])
-    print(peticion)
+    print(f"{peticion}\n")
     return json.loads(peticion.content)
 
-# Función para hacer la petición a la API del Perfil Falso e imprimir su contenido
-def fakeProfile(urls: list):
-    
-    # Se declaran e inicializan las variables
-    listaImprimir: list = []
-    perfilFalso : str
-
-    # Se hace la petición y se carga el contenido de la API
-    print(f"\n{urls[2]}")
+# Se hace la petición a la API de gatos, se imprime el JSON y se entrega el contenido
+def gatos(urls: list):
+    print(f"\n{urls[2]}\n")
     peticion = requests.get(urls[2])
-    print(peticion)
-    perfilFalso = json.loads(peticion.content)
-
-    # Se imprimen los datos del usuario
-    print(f"\nLos datos de tu nuevo usuario son:\nGénero: {perfilFalso['results'][0]['gender']}")
-
-    # Bucle para imprimir cada parte del nombre en la misma línea sin estar en una lista
-    for seccion in perfilFalso['results'][0]['name']:
-        listaImprimir.append(perfilFalso['results'][0]['name'][seccion])
-    print(f"Nombre completo: {listaImprimir[0]} {listaImprimir[1]} {listaImprimir[2]}")
-
-    # Se obtiene la fecha de nacimiento en la API
-    fecha = perfilFalso['results'][0]['dob']['date']
-    # Se convierte el string a un objeto datetime
-    fecha = datetime.fromisoformat(fecha.replace("Z", "+00:00"))
-    # Se formatea la fecha en un formato legible
-    fecha = fecha.strftime("%d of %B of %Y, %H:%M:%S")
-    # Se imprime el resultado y la edad actual
-    print(f"Fecha de Nacimiento: {fecha} Edad: {perfilFalso['results'][0]['dob']['age']} años")
-
-    listaImprimir = [] # Se reinicializa la variable
-    for seccion in perfilFalso['results'][0]['location']['street']:
-        listaImprimir.append(perfilFalso['results'][0]['location']['street'][seccion])
-    print(f"Dirección: {listaImprimir[0]} {listaImprimir[1]}\nUbicación:") # Se imprime los datos de la dirección
-    for seccion in perfilFalso['results'][0]['location']:
-        # Se imprime la ubicación, omitiendo dirección, coordenadas, zona horario y descripción
-        if seccion == 'street' or seccion == 'coordinates' or seccion == 'timezone' or seccion == 'description':
-            continue
-        print(perfilFalso['results'][0]['location'][seccion])
-
-    # Se obtiene e imprime el email falso
-    email = perfilFalso['results'][0]['email']
-    print(f"Email: {email}")
-
-    # Se obtienen e imprimen el usuario y contraseña falsos
-    user = perfilFalso['results'][0]['login']['username']
-    pw = perfilFalso['results'][0]['login']['password']
-    print(f"Usuario: {user}")
-    print(f"Contraseña: {pw}\n")
-
+    print(f"{peticion}\n")
+    return json.loads(peticion.content)
 
 def ImprimirAPIs(urls): # Función madre para llamar a las demás e imprimir resultados
     
-    # Obtiene la broma e imprime el inicio y el remate por separado
+    # Obtiene el contenido del JSON de bromas e imprime el conjunto de llaves y valores
     bromita : str = broma(urls)
-    print(f"\nTop 10 mejores bromas del siglo 21:\n{bromita['setup']}\n {bromita['punchline']}\n")
+    print(bromita)
+    for k,v in bromita.items():
+        print(k, "->", v)
     
-    # Obtiene los datos del bitcoin e imprime cuándo se consultaron y la conversión a varias monedas
-    bitcoin : str = bitcoinData(urls)
-    print(f"\nActualmente: {bitcoin['time']['updated']}, el {bitcoin['chartName']} equivale a:")
-    for moneda in bitcoin['bpi']:
-        print(f"{moneda}: {bitcoin['bpi'][moneda]['rate']} {bitcoin['bpi'][moneda]['description']}")
+    # Obtiene el contenido del JSON de fotos de perros e imprime el conjunto de llaves y valores
+    fotoPerro : str = perros(urls)
+    print(fotoPerro)
+    for k,v in fotoPerro.items(): 
+        print(k, "->", v)
 
-    # Se llama a la función del perfil falso donde se imprimen también los resultados
-    fakeProfile(urls)
-
+    # Obtiene el contenido del JSON de datos de gatos e imprime el conjunto de llaves y valores
+    datosGatos : str = gatos(urls)
+    print(datosGatos)
+    for k,v in datosGatos.items(): 
+        print(k, "->", v)
 
 if __name__ == "__main__": # Función main para dar inicio al código
 
     # Se declaran e inicializan las url
     urlBroma : str = "https://official-joke-api.appspot.com/random_joke"
-    urlBitcoin : str = "https://api.coindesk.com/v1/bpi/currentprice.json"
-    urlFakeUser : str = "https://randomuser.me/api/"
-    urls: list = [urlBroma, urlBitcoin, urlFakeUser] # Se guardan en una lista
+    urlPerros : str = "https://dog.ceo/api/breeds/image/random"
+    urlGatos : str = "https://catfact.ninja/fact"
+    urls: list = [urlBroma, urlPerros, urlGatos] # Se guardan en una lista
     ImprimirAPIs(urls) # Se llama a la función madre
